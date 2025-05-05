d5c46da2c78a0ec93653a9ec40b0b229c68b9bc9
Leonan Reis <146787577+LeonanAraudo@users.noreply.github.com>
2025-05-05 19:48:37 -0300

    Update get-summary.ts

--- src/http/get-summary.ts
+++ src/http/get-summary.ts
@@ -11,7 +11,7 @@ type SummaryResponse = {
   >
 }
 export async function getSummary(): Promise<SummaryResponse> {
-  const response = await fetch('http://localhost:3333/summary')
+  const response = await fetch('https://backend-gerenciadordemetas.onrender.com/summary')
   const data = await response.json()
   return data.summary
 }
