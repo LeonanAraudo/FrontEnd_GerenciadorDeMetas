a0554e57074a439f29f2c0c57b6281c8384b41f6
Leonan Reis <146787577+LeonanAraudo@users.noreply.github.com>
2025-05-07 16:12:29 -0300

    Update pending-goals.tsx

--- src/components/ui/pending-goals.tsx
+++ src/components/ui/pending-goals.tsx
@@ -3,13 +3,18 @@ import { OutlineButton } from './outline-button'
 import { getPendingGoals } from '../../http/get-pending-goals'
 import { useQuery, useQueryClient } from '@tanstack/react-query'
 import { createGoalCompletion } from '../../http/create-goal-completion'
-
+import { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { getSummary } from '../../http/get-summary'
 export function PendingGoals() {
+  const navigate = useNavigate()
+
   const queryClient = useQueryClient()
+
   const { data } = useQuery({
-    queryKey: ['pendding-goals'],
-    queryFn: getPendingGoals, //Qual função vou executar para trazer os dados
-    staleTime: 1000 * 60, //Cria um cash com os dados, a pesquisa dos dados será feita a cada tempo selecionado
+    queryKey: ['pending-goals'], // Corrigido o erro de digitação
+    queryFn: getPendingGoals, // Qual função vai executar para trazer os dados
+    staleTime: 1000 * 60, // Cria um cache com os dados, pesquisa dos dados será feita a cada tempo selecionado
   })
 
   if (!data || !Array.isArray(data)) {
@@ -18,8 +23,30 @@ export function PendingGoals() {
 
   async function handleCompletionGoal(goalId: string) {
     await createGoalCompletion(goalId)
-    queryClient.invalidateQueries({ queryKey: ['summary'] }) // vantagens do reactquery isso aqui permite que ao clicar na meta não precise atualizar a pagina para que ele seja inserido nas metas comcluidas
-    queryClient.invalidateQueries({ queryKey: ['pendding-goals'] })
+    queryClient.invalidateQueries({ queryKey: ['summary'] }) // Atualiza as metas concluídas
+    queryClient.invalidateQueries({ queryKey: ['pending-goals'] }) // Atualiza as metas pendentes
+  }
+
+  const { data: summaryData } = useQuery({
+    queryKey: ['summary'],
+    queryFn: getSummary,
+  })
+
+  useEffect(() => {
+    if (summaryData) {
+      const completed = summaryData?.completed ?? 0
+      const total = summaryData?.total ?? 0
+
+      if (completed === total && total > 0) {
+        // Redireciona pra tela inicial
+        navigate('/')
+      }
+    }
+  }, [summaryData, navigate]) // Corrigido para usar "navigate" em vez de "router"
+
+  // Se tudo foi completado, evita até mostrar as metas
+  if (summaryData?.completed === summaryData?.total) {
+    return null
   }
 
   return (
